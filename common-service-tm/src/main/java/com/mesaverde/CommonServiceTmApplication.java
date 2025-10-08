c33242a73f631448f7ed80eac00071ca54fa9259
mercedesBit <mercedes.mantari09@gmail.com>
2025-10-08 17:37:27 -0500

    Modificacion del common porque solo deberia ser un proyecto java mas no spring boot

--- common-service-tm/src/main/java/com/mesaverde/CommonServiceTmApplication.java
+++ common-service-tm/src/main/java/com/mesaverde/CommonServiceTmApplication.java
@@ -1,13 +1,11 @@
 package com.mesaverde;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CommonServiceTmApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CommonServiceTmApplication.class, args);
-	}
-
+public class CommonServiceTmApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Proyecto!" );
+    }
 }
+
--- common-service-tm/src/main/java/com/mesaverde/entity/Cliente.java
+++ common-service-tm/src/main/java/com/mesaverde/entity/Cliente.java
@@ -8,8 +8,6 @@ import lombok.Setter;
 
 @Entity
 @Table(name = "clientes")
-@Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Cliente {
@@ -21,5 +19,35 @@ public class Cliente {
     private String email;
     private String telefono;
     private String direccion;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
 
 }
